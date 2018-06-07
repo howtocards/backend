5ca102366dcff5fbb1b6a7f2795cfc7f373124fc
Sergey Sova <mail@sergeysova.com>
2018-06-07 23:46:11 +0300

    feat(simple index):

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,27 @@
 extern crate actix_web;
 use actix_web::{server, App, HttpRequest, Responder};
+use std::cell::Cell;
 
-fn index(_req: HttpRequest) -> impl Responder {
-    "Foo"
+struct AppState {
+    counter: Cell<usize>,
+}
+
+fn index(req: HttpRequest<AppState>) -> impl Responder {
+    let count = req.state().counter.get() + 1;
+
+    req.state().counter.set(count);
+    format!("Request number: {}", count)
 }
 
 pub fn create_server() {
-    server::new(|| App::new().resource("/", |r| r.f(index)))
-        .bind("127.0.0.1:9000")
-        .unwrap()
-        .run();
+    let app = server::new(|| {
+                              App::with_state(AppState { counter: Cell::new(0) }).resource("/",
+                                                                                     |r| r.f(index))
+                          })
+            .workers(2)
+            .bind("127.0.0.1:9000")
+            .expect("Can not bind to 127.0.0.1:9000s");
+
+    println!("Server started on http://127.0.0.1:9000");
+    app.run();
 }
