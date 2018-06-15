a97d529e640d89083c3d5e3384ca87c86c253886
Sergey Sova <mail@sergeysova.com>
2018-06-15 23:35:32 +0300

    feat(db): implement saving and loading

--- src/db.rs
+++ src/db.rs
@@ -23,12 +23,44 @@ pub struct User {
     password: String,
 }
 
-#[derive(Serialize, Deserialize)]
+impl Default for User {
+    fn default() -> User {
+        User {
+            id: 0,
+            email: String::new(),
+            password: String::new(),
+        }
+    }
+}
+
+impl User {
+    fn new<E: Into<String>>(id: u32, email: E) -> User {
+        User {
+            id,
+            email: email.into(),
+            ..Default::default()
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Database {
     users: BTreeMap<u32, User>,
 }
 
 static FILE_PATH: &'static str = "/tmp/howtocards.db.ron";
+
+fn open_db_file() -> Result<File, DatabaseError> {
+    if let Ok(file) = File::open(FILE_PATH) {
+        Ok(file)
+    } else if let Ok(file) = File::create(FILE_PATH) {
+        Ok(file)
+    } else {
+        Err(DatabaseError::SaveError {
+            reason: String::from("cannot open file"),
+        })
+    }
+}
 
 impl Database {
     fn new() -> Database {
@@ -37,38 +69,48 @@ impl Database {
         }
     }
 
-    fn open() -> Result<File, DatabaseError> {
-        if let Ok(file) = File::open(FILE_PATH) {
-            Ok(file)
-        } else if let Ok(file) = File::create(FILE_PATH) {
-            Ok(file)
-        } else {
-            Err(DatabaseError::SaveError {
-                reason: String::from("cannot open file"),
-            })
-        }
-    }
-
     fn save(&self) -> Result<(), DatabaseError> {
-        Database::open()?;
+        open_db_file()?;
         let stringified = ron::ser::to_string(&self).map_err(|_| DatabaseError::SaveError {
             reason: String::from("cannot serialize db"),
         })?;
 
         fs::write(FILE_PATH, stringified).map_err(|_| DatabaseError::SaveError {
-            reason: String::from("cannot write to a file")
+            reason: String::from("cannot write to a file"),
         })?;
 
         Ok(())
     }
 
     fn load() -> Result<Database, DatabaseError> {
-        Ok(Database::new())
+        use std::io::Read;
+
+        let mut file = open_db_file()?;
+
+        let mut buf = String::new();
+        file.read_to_string(&mut buf).map_err(|_| DatabaseError::LoadError {
+            reason: String::from("cannot read file to string"),
+        })?;
+        let parsed: Database = ron::de::from_str(&buf.as_ref()).map_err(|_| DatabaseError::LoadError {
+            reason: String::from("invalid database file"),
+        })?;
+
+        Ok(parsed)
     }
 }
 
 pub(crate) fn test_db() -> Result<(), Error> {
-    let mut db = Database::new();
+    let mut db = Database::load()?;
+
+    println!("{:?}", db);
+
+    if let Some(_) = db.users.get(&0) {
+        db.users.remove(&0);
+    } else {
+        db.users.insert(0, User::new(0, "Foo"));
+    }
+
+    println!("{:?}", db);
 
     db.save()?;
 
