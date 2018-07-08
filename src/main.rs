76e5e990558e8b621ab0918e919c41cf1429796e
Sergey Sova <mail@sergeysova.com>
2018-07-08 16:05:21 +0300

    fix: change lib/main result names

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 extern crate failure;
-extern crate howtocards_backend;
+extern crate howtocards;
 
 fn main() -> Result<(), failure::Error> {
-    howtocards_backend::create_server()?;
+    howtocards::create_server()?;
     // .bind("127.0.0.1:9000")
     // .unwrap()
     // .run();
