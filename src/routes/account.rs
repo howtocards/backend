3aaa1c9449089ab36ac61076e8a1ed5801547a4d
Sergey Sova <i.am@lestad.net>
2018-09-27 17:47:15 +0300

    feat: add session getter

--- src/routes/account.rs
+++ src/routes/account.rs
@@ -1,5 +1,6 @@
 //! /account
 
+use serde::Serialize;
 use actix::prelude::*;
 use actix_web::dev::HttpResponseBuilder;
 use actix_web::Error;
@@ -15,6 +16,7 @@ use handlers::account::create::*;
 use handlers::account::login::*;
 use hasher;
 use layer::SuccessAnswer;
+use auth::Auth;
 
 /// POST /account
 pub fn create((account, req): (Json<AccountCreate>, Req)) -> FutureResponse<HttpResponse> {
@@ -54,11 +56,22 @@ pub fn login((login_data, req): (Json<SessionCreate>, Req)) -> FutureResponse<Ht
         .responder()
 }
 
+/// GET /account/session
+pub fn get_session((auth, _req): (Auth, Req)) -> Json<impl Serialize> {
+    #[derive(Serialize)]
+    struct R {
+        email: String,
+    }
+
+    Json(R { email: auth.user.email.clone() })
+}
+
 #[inline]
 pub fn with_app(app: App<AppState>) -> App<AppState> {
     app.resource("/account", |r| {
         r.method(http::Method::POST).with(self::create)
     }).resource("/account/session", |r| {
-        r.method(http::Method::POST).with(self::login)
+        r.method(http::Method::POST).with(self::login);
+        r.method(http::Method::GET).with(self::get_session)
     })
 }
