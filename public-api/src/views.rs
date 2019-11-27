12de38fac22778f59a6107dba7e0130fa42ab65a
Sergey Sova <mail@sergeysova.com>
2019-11-27 10:37:48 +0300

    refactor: add view for card. without implementation

--- public-api/src/views.rs
+++ public-api/src/views.rs
@@ -2,7 +2,7 @@ use chrono::NaiveDateTime;
 
 /// Serialization for User model
 /// Without password field
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct EncodableUserPrivate {
     pub display_name: Option<String>,
@@ -14,7 +14,7 @@ pub struct EncodableUserPrivate {
 
 /// Serialization for User model
 /// Same as Private except without email
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct EncodableUserPublic {
     pub display_name: Option<String>,
@@ -24,7 +24,7 @@ pub struct EncodableUserPublic {
 }
 
 /// User settings to communicate with frontend
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize)]
 #[serde(rename_all = "camelCase")]
 pub struct UserSettings {
     pub display_name: Option<String>,
@@ -33,7 +33,7 @@ pub struct UserSettings {
     pub username: String,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize)]
 #[serde(rename_all = "camelCase")]
 pub struct CardMeta {
     pub title: String,
@@ -42,4 +42,35 @@ pub struct CardMeta {
     pub created_at: Option<NaiveDateTime>,
     pub updated_at: Option<NaiveDateTime>,
     pub preview: Option<String>,
+}
+
+#[derive(Debug, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Permissions {
+    pub is_useful: bool,
+    pub can_edit: bool,
+}
+
+#[derive(Debug, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Card {
+    pub id: i32,
+    pub author_id: i32,
+    pub title: String,
+    pub content: CardContent,
+    pub created_at: NaiveDateTime,
+    pub updated_at: NaiveDateTime,
+    /// Count of users, that added card to its library
+    pub useful_for: i64,
+    pub permissions: Permissions,
+    pub tags: Vec<String>,
+    pub preview_url: Option<String>,
+}
+
+#[derive(Debug, Serialize)]
+#[serde(untagged)]
+pub enum CardContent {
+    /// Source returned only if card is not rendered yet
+    Source(serde_json::Value),
+    Rendered(String),
 }
